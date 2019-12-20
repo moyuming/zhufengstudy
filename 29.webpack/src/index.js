67972ef2552d20c06de760912eec3e2516ca7795
moyumin <moyumin@simuwang.com>
2019-12-20 18:20:33 +0800

    add

--- 29.webpack/src/index.js
+++ 29.webpack/src/index.js
@@ -4,5 +4,10 @@ button.onclick = function() {
     import(/*webpackChunkName: 'title'*/'./title.js').then(function(result){
         console.log(result.default);
     });
+    //异步加载
+    /*require.ensure([], function(){//当title.js需要依赖于其他模块的时候，需要在[]中加入
+    let result = require('./title.js')
+        console.log(result);
+    })*/
 };
 document.body.appendChild(button);
